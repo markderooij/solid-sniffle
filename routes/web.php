938878b56124a0d3f7649874d1dd72eda4ba6ba9
Mark de Rooij <mm.derooij@gmail.com>
2022-09-12 12:16:10 +0200

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $planets = [
+        [
+            'name' => 'Mars',
+            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
+        ],
+        [
+            'name' => 'Venus',
+            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
+        ],
+        [
+            'name' => 'Earth',
+            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far thats inhabited by living things.'
+        ]
+    ];
+    return view('welcome', ['planets' => $planets]);
+});
+
+
+Route::get('/b', function () {
+    return [
+        'Mars', 
+        'Aarde', 
+        'Venus', 
+        'Neptunus', 
+        'Uranus'
+    ];
+});
